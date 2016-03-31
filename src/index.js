9e782fb559c7671dd95b1d6eccb9b6136c3beb10
Duong Tran <tq.duong@icloud.com>
2016-03-31 11:29:34 +0700

    Adding sass preprocessor

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// import scss
+import '../style/style.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel'
-    }]
+    loaders: [
+      { exclude: /node_modules/, loader: 'babel' },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
